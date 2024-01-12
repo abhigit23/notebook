617a21af5111abf440eafbf2cd4412d486dd5e85
abhigit23 <abhijeetbhandari84@gmail.com>
2024-01-12 18:35:25 +0530

    Added vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+			manifest: {
+				name: "Notebook",
+				short_name: "Notebook",
+				description: "Your book to take notes",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "vite.svg",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "vite.svg",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
